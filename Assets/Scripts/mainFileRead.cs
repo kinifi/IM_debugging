a196e58d1ce8ea7ef4c245460b41e7ca0fe6cbfe
Chris Figueroa <chrisfigueroa16@gmail.com>
2014-06-23 20:52:15 -0500

    Full Commit

---
+++ Assets/Scripts/mainFileRead.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainFileRead : MonoBehaviour {
+
+	public bool saveFile = false, statsFile = false, debugFile = false;
+	public GUISkin skin;
+	private string fileLocation;
+
+	// Use this for initialization
+	void Start () {
+	
+		fileLocation = Application.dataPath + "/" + "Debugfiles" + "/";
+
+		if(checkIfFilesExist("Debug.txt"))
+			debugFile = true;
+
+		if(checkIfFilesExist("stats.txt"))
+			statsFile = true;
+
+		if(checkIfFilesExist("imsave.txt"))
+			saveFile = true;
+
+	}
+
+	private bool checkIfFilesExist(string fileName) {
+
+		if(ES2.Exists(fileLocation + fileName))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+	void OnGUI () {
+
+		GUI.skin = skin;
+
+		GUILayout.BeginArea(new Rect( 10, 10, Screen.width/2, Screen.height));
+		GUILayout.Label("Debug File Information");
+		debugFileInformation();
+
+		GUILayout.Label("Stats File Information");
+		statsFileInformation();
+
+		GUILayout.Label("Save File Information");
+		saveFileInformation();
+
+
+		GUILayout.EndArea();
+	}
+
+	private void saveFileInformation() {
+
+		if(!saveFile)
+		{
+			GUILayout.Label("Save File .txt does not exist in the folder");
+			GUILayout.Space(10);
+		}
+		else
+		{
+			int _test;
+			_test = ES2.Load<int>(fileLocation + "imsave" + ".txt" + "?tag=levelUnlock");
+			
+			GUILayout.Label("tag: levelUnlock: " + _test);
+
+		}
+	}
+
+	private void statsFileInformation() {
+
+		if(!statsFile)
+		{
+			GUILayout.Label("Stats File .txt does not exist in the folder");
+			GUILayout.Space(10);
+		}
+		else
+		{
+			int _stat1;
+			int _stat2;
+			int _stat3;
+			int _stat4;
+
+			_stat1 = ES2.Load<int>(fileLocation + "stats" + ".txt" + "?tag=deathBySpike");
+			_stat2 = ES2.Load<int>(fileLocation + "stats" + ".txt" + "?tag=deathByFalling");
+			_stat3 = ES2.Load<int>(fileLocation + "stats" + ".txt" + "?tag=trampolineJumps");
+			_stat4 = ES2.Load<int>(fileLocation + "stats" + ".txt" + "?tag=normalJumps");
+
+
+			if(_stat1 != null)
+			{
+				GUILayout.Label("tag: deathBySpike: " + _stat1);
+				GUILayout.Label("tag: deathByFalling: " + _stat2);
+				GUILayout.Label("tag: trampolineJumps: " + _stat3);
+				GUILayout.Label("tag: normalJumps: " + _stat4);
+
+			}
+		}
+
+	}
+
+	private void debugFileInformation () {
+
+		if(!debugFile)
+		{
+			GUILayout.Label("Debug File .txt does not exist in the folder");
+			GUILayout.Space(10);
+		}
+		else
+		{
+			string _test;
+			_test = ES2.Load<string>(fileLocation + "Debug" + ".txt" + "?tag=achievement");
+
+			GUILayout.Label("tag: achievement: " + _test);
+		}
+	}
+
+}
---
+++ Assets/Scripts/mainPrompt.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class mainPrompt : MonoBehaviour {
+
+	private bool hasPlaceFiles = false;
+
+	// Use this for initialization
+	void Start () {
+	
+		if(ES2.Exists(Application.dataPath + "/" + "Debugfiles"))
+		{
+			Debug.Log("File Exists");
+		}
+		else
+		{
+			ES2.Save(123, Application.dataPath + "/Debugfiles/dummyfile.txt");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+
+		GUILayout.BeginArea(new Rect(Screen.width/2-200, Screen.height-100, 200, 200));
+		if(GUILayout.Button("Open Folder"))
+		{
+			OpenInFileBrowser(Application.dataPath + "/" + "Debugfiles");
+			hasPlaceFiles = true;
+		}
+		if(hasPlaceFiles)
+		{
+			if(GUILayout.Button("Read Files"))
+			{
+				Debug.Log("Go Read Files");
+				Application.LoadLevel("scene2");
+			}
+		}
+
+		GUILayout.EndArea();
+	}
+
+	public static void OpenInMacFileBrowser(string path)
+	{
+		bool openInsidesOfFolder = false;
+		
+		// try mac
+		string macPath = path.Replace("\\", "/"); // mac finder doesn't like backward slashes
+		
+		if (Directory.Exists(macPath)) // if path requested is a folder, automatically open insides of that folder
+		{
+			openInsidesOfFolder = true;
+		}
+		
+		//Debug.Log("macPath: " + macPath);
+		//Debug.Log("openInsidesOfFolder: " + openInsidesOfFolder);
+		
+		if (!macPath.StartsWith("\""))
+		{
+			macPath = "\"" + macPath;
+		}
+		if (!macPath.EndsWith("\""))
+		{
+			macPath = macPath + "\"";
+		}
+		string arguments = (openInsidesOfFolder ? "" : "-R ") + macPath;
+		//Debug.Log("arguments: " + arguments);
+		try
+		{
+			System.Diagnostics.Process.Start("open", arguments);
+		}
+		catch(System.ComponentModel.Win32Exception e)
+		{
+			// tried to open mac finder in windows
+			// just silently skip error
+			// we currently have no platform define for the current OS we are in, so we resort to this
+			e.HelpLink = ""; // do anything with this variable to silence warning about not using it
+		}
+	}
+	
+	public static void OpenInWinFileBrowser(string path)
+	{
+		bool openInsidesOfFolder = false;
+		
+		// try windows
+		string winPath = path.Replace("/", "\\"); // windows explorer doesn't like forward slashes
+		
+		if (Directory.Exists(winPath)) // if path requested is a folder, automatically open insides of that folder
+		{
+			openInsidesOfFolder = true;
+		}
+		try
+		{
+			System.Diagnostics.Process.Start("explorer.exe", (openInsidesOfFolder ? "/root," : "/select,") + winPath);
+		}
+		catch(System.ComponentModel.Win32Exception e)
+		{
+			// tried to open win explorer in mac
+			// just silently skip error
+			// we currently have no platform define for the current OS we are in, so we resort to this
+			e.HelpLink = ""; // do anything with this variable to silence warning about not using it
+		}
+	}
+	
+	public static void OpenInFileBrowser(string path)
+	{
+		OpenInWinFileBrowser(path);
+		OpenInMacFileBrowser(path);
+	}
+
+}
